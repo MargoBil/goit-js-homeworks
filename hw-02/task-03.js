2b834e9a855089ddce381396e83f1f67a946d3d7
MargoBil <margo10011990@gmail.com>
2020-01-25 16:43:35 +0200

    finish work

--- hw-02/task-03.js
+++ hw-02/task-03.js
@@ -11,8 +11,8 @@ const findLongestWord = function (string) {
   return maxWord;
 };
 
-console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+console.log (findLongestWord ('The quick brown fox jumped over the lazy dog')); // 'jumped'
 
-console.log(findLongestWord('Google do a roll')); // 'Google'
+console.log (findLongestWord ('Google do a roll')); // 'Google'
 
-console.log(findLongestWord('May the force be with you')); // 'force'
+console.log (findLongestWord ('May the force be with you')); // 'force'
