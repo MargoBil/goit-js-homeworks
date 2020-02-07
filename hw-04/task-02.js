34ae52072297d141faab24388a8430449ddf0b11
MargoBil <margo10011990@gmail.com>
2020-02-07 22:34:32 +0200

    hw-04

---
+++ hw-04/task-02.js
@@ -0,0 +1,30 @@
+'use strict';
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add (itemName) {
+    console.log (`Adding ${itemName} to inventory`);
+    this.items.push (itemName);
+  },
+  remove (itemName) {
+    console.log (`Removing ${itemName} from inventory`);
+    this.items = this.items.filter (item => item !== itemName);
+  },
+};
+const boundAdd = inventory.add.bind (inventory);
+const boundRemove = inventory.remove.bind (inventory);
+const invokeInventoryAction = function (itemName, action) {
+  console.log (`Invoking action on ${itemName}`);
+  action (itemName);
+};
+
+invokeInventoryAction ('Medkit', boundAdd);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log (inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction ('Gas mask', boundRemove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log (inventory.items); // ['Knife', 'Medkit']
